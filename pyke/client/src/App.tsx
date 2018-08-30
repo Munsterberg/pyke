929d81b0f3bd3c1f98d291254afbe5a618d114c9
Munsterberg <jetrussell93@gmail.com>
2018-08-30 09:21:02 -0400

    wait for parcel 2 i guess ¯\_(ツ)_/¯

--- pyke/client/src/App.tsx
+++ pyke/client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 
-class App extends React.Component {
+export default class App extends React.Component {
   public render() {
     return <div>ok</div>;
   }
